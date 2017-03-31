8b997218af4f0b25b7e98fc40919b561043de2d8
“Zach” <bigrabbit716@gmail.com>
2017-03-31 16:42:56 +0800

    Initial Commit

---
+++ homework1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework1
+//
+//  Created by Zach on 2017/3/31.
+//  Copyright © 2017年 Zach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
